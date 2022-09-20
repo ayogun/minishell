27643b8cbba5ed00ee0d14bb94cb6fd2db3ab313
A.YOGUN <yogun@student.42heilbronn.de>
2022-09-20 16:58:34 +0200

    new env node creation function has been added

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,28 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 18:45:59 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/20 16:41:07 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/20 16:52:05 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+t_env	*ft_new_env(char *s)
+{
+	t_env	*new;
+	int		i;
+
+	new = malloc(sizeof(t_env));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s[i] && s[i] != '=')
+		i++;
+	new->key = ft_substr(s, 0, i);
+	new->value = ft_strdup(s + i + 1);
+	new->next = NULL;
+	return (new);
+}
 
 // Here I will take env and tokenize them with linked list.
 void tokenize_env(t_data *data)
