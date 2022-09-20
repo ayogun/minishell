e2a03ded6dbbe1ee5c876bcb5c94183bac77faab
A.YOGUN <yogun@student.42heilbronn.de>
2022-09-20 16:50:35 +0200

    linked list struct has been added to the library for env variables

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 18:47:29 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/20 16:21:21 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/20 16:48:20 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,14 @@ typedef struct s_data
 	char	**argv;
 	char	**env;
 }				t_data;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}					t_env;
+
+void tokenize_env(t_data *data);
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,17 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 18:45:59 by yogun             #+#    #+#             */
-/*   Updated: 2022/09/20 16:22:16 by yogun            ###   ########.fr       */
+/*   Updated: 2022/09/20 16:41:07 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+// Here I will take env and tokenize them with linked list.
+void tokenize_env(t_data *data)
+{
+	
+}
 
 int	main(int argc, char **argv, char **envp)
 {	
@@ -20,6 +26,8 @@ int	main(int argc, char **argv, char **envp)
 	{
 		data.argv = argv;
 		data.env = envp;
+		//I will parse env variables 
+		tokenize_env(&data);
 		write(1,"$",1);
 		while(1)
 			sleep(10);
